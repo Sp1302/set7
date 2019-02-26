21bfa87d7da4c7a368c1ea77aba92e7424a07c66
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-26 12:44:09 +0530

    Create 69.c

---
+++ 69.c
@@ -0,0 +1,14 @@
+# set7
+#include <stdio.h>
+#include<string.h>
+void main() 
+{
+	int n,m,sum;
+	scanf("%d %d\n",&n,&m);
+	sum=n-m;
+	if(sum%2==0)
+	printf("even\n");
+	else
+	printf("odd\n");
+	getch();
+}
