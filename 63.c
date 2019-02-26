a3e00a388aefef5954c29dcd1cd23b5eaa9dc292
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-26 12:27:22 +0530

    Create 63.c

---
+++ 63.c
@@ -0,0 +1,23 @@
+# set7
+#include <stdio.h>
+#include<string.h>
+void main() 
+{
+	char a[100];
+	int n,i,count=0;
+	gets(s);
+	n=strlen(a);
+	for(i=0;i<n;i++)
+	{
+	    if(a[i]==' ')
+	    {   
+	    count++;
+	    }
+	    else
+	    {
+	     continue;
+	    }
+	}
+    printf("%d\n",count+1);
+getch();
+}
