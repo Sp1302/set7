768d31d2f3d200dcaee3827b0781e7d7d433fc6a
Sp1302 <43338163+Sp1302@users.noreply.github.com>
2019-02-26 12:29:35 +0530

    Create 64.c

---
+++ 64.c
@@ -0,0 +1,21 @@
+# set7
+
+#include <stdio.h>
+int main() 
+{
+	int n1,n2,sum;
+	printf("\nenter the number1:");
+	scanf("%d",&n1);
+	printf("\nenter the number2:");
+	scanf("%d\n",&n2);
+	sum=n1+n2;
+	if(sum%2==0)
+	{
+		printf("\neven");
+	}
+	else
+	{
+		printf("\nodd");
+	}
+	return 0;
+}
